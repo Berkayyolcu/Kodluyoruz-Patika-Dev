da50cc9de7396a9f3877ba234211fac7eab8513e
Berkay <56292568+Berkayyolcu@users.noreply.github.com>
2023-02-18 04:38:54 +0300

    Add files via upload

---
+++ C#/tanımlama_erişim_ve_döngülerle_dizi_kullanımı/tanımlama_erişim_ve_döngülerle_dizi_kullanımı/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tanımlama_erişim_ve_döngülerle_dizi_kullanımı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Dizi tanımlama
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+
+
+
+            // Dizilere Değer Atama ve Erişim
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[1]); // Köpek
+            Console.WriteLine(dizi[3]); // 10
+            Console.WriteLine(renkler[0]); // Mavi
+
+
+
+
+            //Döngülerle Dizi kullanımı
+            // klavyeden girilen n tane sayının ortalamasını hesaplayan program
+
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz :");
+            int diziuzunlugu = int.Parse(Console.ReadLine());
+            int[] sayidizisi = new int[diziuzunlugu];
+
+            for(int i=0; i<diziuzunlugu; i++)
+            {
+                Console.WriteLine("Lütfen {0}. sayısı giriniz",i+1);
+                sayidizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+            foreach (var sayi in sayidizisi) 
+                toplam += sayi;
+
+
+            Console.WriteLine("Ortalama : " + toplam/diziuzunlugu);
+            
+
+        }
+    }
+}
---
+++ C#/tanımlama_erişim_ve_döngülerle_dizi_kullanımı/tanımlama_erişim_ve_döngülerle_dizi_kullanımı/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("tanımlama_erişim_ve_döngülerle_dizi_kullanımı")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tanımlama_erişim_ve_döngülerle_dizi_kullanımı")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("b1b8f9c2-f2d2-456c-8736-607490e18598")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
