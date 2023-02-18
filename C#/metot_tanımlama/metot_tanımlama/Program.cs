05449d4ce1ae6c608455ff8a10af56f32c13fde5
Berkay <56292568+Berkayyolcu@users.noreply.github.com>
2023-02-18 15:29:42 +0300

    Add files via upload

---
+++ C#/metot_tanımlama/metot_tanımlama/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metot_tanımlama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // erisim_belirteci geri_dönüs_tipi metot adi(parametrelistesi/arguman)
+            /*
+            {
+             komutlar;
+            }
+            */
+
+            int a = 2;
+            int b = 3;
+
+            Console.WriteLine(a+b); // 5
+
+            
+            int sonuc = Topla(a, b); // a:2 ve b:3 
+            Console.WriteLine("Topla Metotu : "+sonuc); // a+b=5
+
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(" Metotlar Classı EkranaYazdir : " + Convert.ToString(sonuc));// 5
+
+            int sonuc2 = ornek.ArttırVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(" Metotlar Classı ArttırVeTopla : " + Convert.ToString(sonuc2)); // 7
+            ornek.EkranaYazdir(" Metotlar Classı  : " + Convert.ToString(a+b));  // 7
+
+        }
+
+         static int Topla(int deger1, int deger2)
+        {
+
+            return (deger1+deger2);
+
+        }
+
+    }
+
+
+
+    class Metotlar
+    {
+
+         public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+
+        public int ArttırVeTopla(ref int deger1, ref int deger2)
+        {
+            
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+
+        }
+
+    }
+
+
+}
---
+++ C#/metot_tanımlama/metot_tanımlama/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("metot_tanımlama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("metot_tanımlama")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("19dddd09-c5f4-4e5e-8078-6554e0e7d087")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
